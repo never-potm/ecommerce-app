5e2a7889f945157c0b89cebc06376119e8416679
Suraj Gusain <suraj.gusain15@gmail.com>
2025-02-02 21:18:58 +0530

    added global exception handler to output 400 http response

---
+++ src/main/java/com/sunstar/ecommerce/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.sunstar.ecommerce.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * This method will intercept any exception thrown in this application
+ */
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<Map<String, String>> methodArgumentInvalidException(MethodArgumentNotValidException e) {
+		Map<String, String> response = new HashMap<>();
+
+		e.getBindingResult().getAllErrors().forEach(error -> {
+			String fieldName = ((FieldError)error).getField();
+			String message = error.getDefaultMessage();
+
+			response.put(fieldName, message);
+		});
+
+		return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+	}
+}
