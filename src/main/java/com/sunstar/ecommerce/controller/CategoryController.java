5a573f22bc965e9cca687a8446d84f98bfb2beb6
Suraj Gusain <suraj.gusain15@gmail.com>
2025-02-02 23:38:24 +0530

    cleaned up category controller

--- src/main/java/com/sunstar/ecommerce/controller/CategoryController.java
+++ src/main/java/com/sunstar/ecommerce/controller/CategoryController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -40,22 +39,14 @@ public class CategoryController {
 	@DeleteMapping("/admin/categories/{categoryId}")
 //	@RequestMapping(method = RequestMethod.DELETE, value = "/admin/categories/{categoryId}")
 	public ResponseEntity<String> deleteCategory(@PathVariable Long categoryId) {
-		try {
-			String status = categoryService.deleteCategory(categoryId);
-			return ResponseEntity.status(HttpStatus.OK).body(status);
-		} catch (ResponseStatusException e) {
-			return ResponseEntity.status(e.getStatusCode()).body(e.getReason());
-		}
+		String status = categoryService.deleteCategory(categoryId);
+		return ResponseEntity.status(HttpStatus.OK).body(status);
 	}
 
 	@PutMapping("/public/categories/{categoryId}")
 //	@RequestMapping(method = RequestMethod.PUT, value = "/public/categories/{categoryId}")
-	public ResponseEntity<String> updateCategory(@RequestBody Category category, @PathVariable Long categoryId) {
-		try {
-			Category updatedCategory = categoryService.updateCategory(category, categoryId);
-			return ResponseEntity.status(HttpStatus.OK).body("Category updated");
-		} catch (ResponseStatusException e) {
-			return ResponseEntity.status(e.getStatusCode()).body(e.getReason());
-		}
+	public ResponseEntity<String> updateCategory(@Valid @RequestBody Category category, @PathVariable Long categoryId) {
+		Category updatedCategory = categoryService.updateCategory(category, categoryId);
+		return ResponseEntity.status(HttpStatus.OK).body("Category updated");
 	}
 }
