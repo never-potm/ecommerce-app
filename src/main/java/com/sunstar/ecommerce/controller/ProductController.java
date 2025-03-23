34eab666d5ab9fa1c931eb7f9f7eaf83752e60d4
Suraj Gusain <suraj.gusain15@gmail.com>
2025-03-23 11:09:01 +0530

    added delete product implementation

--- src/main/java/com/sunstar/ecommerce/controller/ProductController.java
+++ src/main/java/com/sunstar/ecommerce/controller/ProductController.java
@@ -45,4 +45,10 @@ public class ProductController {
 		ProductDTO updatedProductDTO = productService.updateProduct(productId, product);
 		return new ResponseEntity<>(updatedProductDTO, HttpStatus.OK);
 	}
+
+	@DeleteMapping("/admin/product/{productId}")
+	public ResponseEntity<ProductDTO> deleteProduct(@PathVariable Long productId) {
+		ProductDTO deletedProductDTO = productService.deleteProduct(productId);
+		return new ResponseEntity<>(deletedProductDTO, HttpStatus.OK);
+	}
 }
